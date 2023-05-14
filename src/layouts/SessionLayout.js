62337384996805a447cd5c8e577fd090d1512b14
boralex <boris.alexandre.rose@gmail.com>
2023-05-14 11:17:14 +0200

    "feat/layout"

---
+++ src/layouts/SessionLayout.js
@@ -0,0 +1,29 @@
+import React from "react"
+import NavBarLeft from "../components/NavBars/NavBarLeft"
+
+
+const SessionLayout = () => {
+
+
+    return(
+
+
+        <>
+
+            
+        
+        
+        
+        
+        </>
+
+
+
+    )
+
+
+
+}
+
+
+export default SessionLayout
